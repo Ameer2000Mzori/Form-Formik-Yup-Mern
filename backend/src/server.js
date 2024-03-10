bb590a77351b03de8707a4461d48b9d31b26827c
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-10 16:43:31 +0100

    added router post

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,32 @@
 import express from 'express'
 
+// adding Router
+const Router = express.Router()
 const app = express()
 
-app.get('/', (req, res) => {
+// data :
+const formData = []
+
+// requests
+Router.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+Router.post('/postform', (req, res) => {
+  const { firstname, lastname, age, gender, country, city, message } = req.body
+  res.send(
+    'data we got : ',
+    firstname,
+    lastname,
+    age,
+    gender,
+    country,
+    city,
+    message
+  )
+})
+
+// server listening
 app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
