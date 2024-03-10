da7757b068b6d5b1c7b647f896d8bf6d5f7ae9ec
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-10 19:32:55 +0100

    finished the project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ToastContainer />
   </QueryClientProvider>
 )
