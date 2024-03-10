746d1751aee5f68fcde781488d430a0a217e1b43
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-10 18:32:16 +0100

    added new file for validation schema

--- frontend/src/component/Form.jsx
+++ frontend/src/component/Form.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useFormik } from 'formik'
-import * as Yup from 'yup'
+import { validationSchema } from './hooks/validationSchema.js'
+
 import {
   StyledForm,
   StyledFormWrap,
@@ -11,27 +12,6 @@ import {
   StyledInputsTextAreaWrap,
   StyledErrorMessage,
 } from './hooks/StyledComponents.jsx'
-
-const validationSchema = Yup.object().shape({
-  firstname: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  lastname: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string().email('Invalid email address').required('Required'),
-  phonenumber: Yup.string().required('Required'),
-  age: Yup.number()
-    .min(0, 'Too Short!')
-    .max(100, 'Too Long!')
-    .required('Required'),
-  gender: Yup.string().required('Required'),
-  country: Yup.string().required('Required'),
-  city: Yup.string().required('Required'),
-  textarea: Yup.string(),
-})
 
 const Form = () => {
   const formik = useFormik({
