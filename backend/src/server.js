60586e955a8c3604b6944ca657693fad07d8d3a5
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-10 16:58:43 +0100

    added more logic to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
+import uniqid from 'uniqid'
+
 // adding Router
 const app = express()
 const Router = express.Router()
@@ -23,16 +25,27 @@ Router.get('/forms', (req, res) => {
 
 Router.post('/postform', (req, res) => {
   const { firstname, lastname, age, gender, country, city, message } = req.body
-  res.send(
-    'data we got : ',
-    firstname,
-    lastname,
-    age,
-    gender,
-    country,
-    city,
-    message
-  )
+  res
+    .status(200)
+    .send(
+      'data we got : ',
+      firstname,
+      lastname,
+      age,
+      gender,
+      country,
+      city,
+      message
+    )
+
+  //   firstname,
+  //   lastname,
+  //   age,
+  //   gender,
+  //   country,
+  //   city,
+  //   message,
+  //   id: uniqid()
 })
 
 // server listening
