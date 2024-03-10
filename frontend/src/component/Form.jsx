bd4aa0dfd4baf073eb57cfa5eb0953d35c536b0c
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-10 17:41:13 +0100

    added all data needed for the form

--- frontend/src/component/Form.jsx
+++ frontend/src/component/Form.jsx
@@ -17,13 +17,45 @@ const Form = () => {
           <label for="lastname">lastname</label>
           <input type="text" name="lastname" id="lastname" />
         </StyledInputsWrap>
+        <div className="flex flex-row text-center items-center justify-between h-[50px] w-[50%]">
+          <div className="flex flex-row text-center items-center gap-2">
+            <label for="age">age</label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              name="age"
+              id="age"
+              className="w-[35px]"
+            />
+          </div>
+          <div className="flex flex-row text-center items-center gap-2">
+            <label for="genderselection"> gender</label>
+            <select id="genderselection">
+              <option value="">male</option>
+              <option value="">female</option>
+            </select>
+          </div>
+        </div>
+
         <StyledInputsWrap>
-          <label for="age">age</label>
-          <input type="number" name="age" id="age" />
+          <label for="country">Country</label>
+          <input type="text" name="country" id="country" />
         </StyledInputsWrap>
+
         <StyledInputsWrap>
-          <label for="firstname">firstname</label>
-          <input type="text" name="firstname" id="firstname" />
+          <label for="city">City</label>
+          <input type="city" name="city" id="city" />
+        </StyledInputsWrap>
+
+        <StyledInputsWrap>
+          <label for="textarea">message...</label>
+          <textarea
+            name="textarea"
+            id="textarea"
+            cols="30"
+            rows="10"
+          ></textarea>
         </StyledInputsWrap>
       </StyledForm>
     </StyledFormWrap>
